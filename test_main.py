51d7a2dd6efb0c1ed61403d38651c4c94d8a91bb
Secpers <85113979+Secpers@users.noreply.github.com>
2024-11-19 18:05:58 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,7 +6,10 @@ from main import min_of_three_vars
 class MinOfThreeVarsTestCase(unittest.TestCase):
 
     def test_min_a(self):
-        // your test here
+        self.assertEqual(min_of_three_vars(1, 2, 3), 1)
 
+    def test_min_b(self):
+        self.assertEqual(min_of_three_vars(2, 1, 3), 1)
 
-        
+    def test_min_c(self):
+        self.assertEqual(min_of_three_vars(3, 2, 1), 1)
